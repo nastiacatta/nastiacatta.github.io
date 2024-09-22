08286afa45fb7ac46aa7c9608007c2d7dc0bbcf6
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-09-22 17:26:58 +0200

    Build and export static site

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 // next.config.js
-
-const nextConfig = {
-    output: 'export', // Enables static export
-    // Other Next.js config options can go here if needed
+module.exports = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
   }
-  
-  module.exports = nextConfig;
   
