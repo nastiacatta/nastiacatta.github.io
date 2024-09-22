9f0ea6319667b896ec445c2fe8c7e2e3d0a5b9f7
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-09-22 17:12:48 +0200

    Configured static export and built project

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+
+const nextConfig = {
+    output: 'export', // Enables static export
+    // Other Next.js config options can go here if needed
+  }
+  
+  module.exports = nextConfig;
+  
