bccf071e9b2b4899756dce3f8c916cf765f4f3c8
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-09-24 20:38:37 +0200

    Deploy updated site

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,41 @@
+// src/components/Projects.jsx
+
 import React from 'react';
 import Link from 'next/link';
 
 export default function Projects() {
+  const projects = [
+    {
+      title: 'EXO GLOVE',
+      href: '/exoglove',
+      imageSrc: '/images/exoglove.jpg', // Path relative to the public folder
+    },
+    {
+      title: 'BIOMORPHUS',
+      href: '/biomorphus',
+      imageSrc: '/images/biomorphus.jpg', // Path relative to the public folder
+    },
+    {
+      title: 'INNOVICE',
+      href: '/innovice',
+      imageSrc: '/images/innovice.jpg', // Path relative to the public folder
+    },
+  ];
+
   return (
     <div className="py-10 px-6">
       <h2 className="text-4xl font-bold mb-6">Projects</h2>
-      <div className="project-title mb-4">
-        <Link href="/exoglove">
-          <a className="text-2xl hover:text-pink-500 transition-colors duration-300">EXO GLOVE</a>
-        </Link>
-      </div>
-      <div className="project-title mb-4">
-        <Link href="/biomorphus">
-          <a className="text-2xl hover:text-pink-500 transition-colors duration-300">BIOMORPHUS</a>
-        </Link>
-      </div>
-      <div className="project-title mb-4">
-        <Link href="/innovice">
-          <a className="text-2xl hover:text-pink-500 transition-colors duration-300">INNOVICE</a>
-        </Link>
+      <div className="project-container">
+        {projects.map((project) => (
+          <Link href={project.href} key={project.title}>
+            <a className="project-card">
+              <img src={project.imageSrc} alt={project.title} />
+              <div className="project-info">
+                <h3>{project.title}</h3>
+              </div>
+            </a>
+          </Link>
+        ))}
       </div>
     </div>
   );
