65eba1697353698ee51614d530f958a8a2728429
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-09-30 20:48:49 +0100

    Deploy updated site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,23 @@
 // src/pages/index.js
 
+import Hero from '../components/Hero';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
 
 export default function Home() {
   return (
     <div>
+      <Hero />
+
       <Header />
 
       {/* Main Content */}
       <main id="main-content">
-        {/* Section 1: Introduction */}
-        <section id="introduction" className="section">
-          <h1 className="text-5xl font-normal mb-4 hover:text-lilac transition-colors">Hello!</h1>
-          <p className="text-xl mb-6">
-            I'm Anastasia, a Design Engineering student with a passion for wearables, AI, and fashion.
-          </p>
-        </section>
-
         {/* Section 2: About Me */}
         <section id="about" className="section">
-          <h2 className="text-4xl font-normal mb-6 hover:text-lilac transition-colors">About Me</h2>
+          <h2 className="text-4xl font-normal mb-6 hover:text-lilac transition-colors">
+            About Me
+          </h2>
           <p className="text-lg">
             I am a third-year MEng Design Engineering student at Imperial College London. My passion lies in the fusion of electronics, AI, and fashion. I am driven by a commitment to integrating elegant design with robust engineering to develop solutions that are both functional and aesthetically pleasing. Beyond my core focus, I have a deep interest in the arts, literature, and architecture, which continually inspire my work.
           </p>
@@ -33,12 +30,15 @@ export default function Home() {
 
         {/* Section 4: Contact */}
         <section id="contact" className="section">
-          <h2 className="text-4xl font-normal mb-6 hover:text-lilac transition-colors">Contact Me</h2>
-          <p className="text-lg mb-4">
-            Feel free to reach out to me at:
-          </p>
+          <h2 className="text-4xl font-normal mb-6 hover:text-lilac transition-colors">
+            Contact Me
+          </h2>
+          <p className="text-lg mb-4">Feel free to reach out to me at:</p>
           <p className="text-lg">
-            <a href="mailto:anastasia.cattaneo@gmail.com" className="underline hover:text-lilac focus:outline-none focus:ring-2 focus:ring-lilac">
+            <a
+              href="mailto:anastasia.cattaneo@gmail.com"
+              className="underline hover:text-lilac focus:outline-none focus:ring-2 focus:ring-lilac"
+            >
               anastasia.cattaneo@gmail.com
             </a>
           </p>
@@ -47,7 +47,9 @@ export default function Home() {
 
       {/* Email in Bottom Right Corner */}
       <div className="email-vertical">
-        <a href="mailto:anastasia.cattaneo@gmail.com" className="email-link">anastasia.cattaneo@gmail.com</a>
+        <a href="mailto:anastasia.cattaneo@gmail.com" className="email-link">
+          anastasia.cattaneo@gmail.com
+        </a>
         <div className="vertical-line"></div>
       </div>
     </div>
