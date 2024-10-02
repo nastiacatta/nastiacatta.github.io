32eb7cceef93bc70ccb071a592e8a1be79f5d557
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-10-03 00:29:05 +0100

    Deploy updated site

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,11 @@
 import Hero from '../components/Hero';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
-import Contact from '../components/Contact'; // Import the Contact component
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Header /> {/* Top menu restored */}
 
       <Hero />
 
@@ -16,7 +15,12 @@ export default function Home() {
       <main id="main-content">
         {/* Section 2: About Me */}
         <section id="about" className="section">
-          {/* Your About Me content */}
+          <h2 className="text-4xl font-normal mb-6 hover:text-lilac transition-colors">
+            About Me
+          </h2>
+          <p className="text-lg">
+            I am a third-year MEng Design Engineering student at Imperial College London. My passion lies in the fusion of electronics, AI, and fashion. I am driven by a commitment to integrating elegant design with robust engineering to develop solutions that are both functional and aesthetically pleasing. Beyond my core focus, I have a deep interest in the arts, literature, and architecture, which continually inspire my work.
+          </p>
         </section>
 
         {/* Section 3: Projects */}
@@ -26,14 +30,25 @@ export default function Home() {
 
         {/* Section 4: Contact */}
         <section id="contact" className="section">
-          <Contact /> {/* Add the Contact component */}
+          <h2 className="text-4xl font-normal mb-6 hover:text-lilac transition-colors">
+            Contact Me
+          </h2>
+          <p className="text-lg mb-4">Feel free to reach out to me at:</p>
+          <p className="text-lg">
+            <a
+              href="mailto:anastasia.cattaneo@gmail.com"
+              className="underline hover:text-lilac focus:outline-none focus:ring-2 focus:ring-lilac"
+            >
+              anastasia.cattaneo@gmail.com
+            </a>
+          </p>
         </section>
       </main>
 
       {/* Email in Bottom Right Corner */}
       <div className="email-vertical">
-        <a href="mailto:aac522@ic.ac.uk" className="email-link">
-          aac522@ic.ac.uk
+        <a href="mailto:anastasia.cattaneo@gmail.com" className="email-link">
+          anastasia.cattaneo@gmail.com
         </a>
         <div className="vertical-line"></div>
       </div>
