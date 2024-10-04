0a71df2d270e4d68892549e1ca3b6bbed27d3650
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-10-05 00:36:11 +0100

    Deploy updated site

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,35 +2,8 @@
 
 import '../styles/globals.css';
 import Head from 'next/head';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const flashlightArea = document.querySelector('.flashlight-area');
-    const flashlight = document.querySelector('.flashlight');
-
-    if (!flashlightArea || !flashlight) return;
-
-    const handleMouseMove = (e) => {
-      const { clientX, clientY } = e;
-      const { left, top } = flashlightArea.getBoundingClientRect();
-
-      // Calculate position relative to the flashlight area
-      const posX = clientX - left;
-      const posY = clientY - top;
-
-      // Update CSS variables for positioning
-      flashlight.style.left = `${posX}px`;
-      flashlight.style.top = `${posY}px`;
-    };
-
-    flashlightArea.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      flashlightArea.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
   return (
     <>
       <Head>
@@ -51,11 +24,6 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:card" content="summary_large_image" />
         {/* Add more meta tags as needed */}
       </Head>
-
-      {/* Flashlight Overlay */}
-      <div className="flashlight-area">
-        <div className="flashlight"></div>
-      </div>
 
       <Component {...pageProps} />
     </>
