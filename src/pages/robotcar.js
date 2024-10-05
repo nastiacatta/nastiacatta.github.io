d54acc086a71a3b926cf75a1c5b96ead246207f8
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-10-05 13:03:46 +0100

    Deploy updated site

--- src/pages/robotcar.js
+++ src/pages/robotcar.js
@@ -1,3 +1,4 @@
+// src/pages/robotcar.js
 import Header from '../components/Header';
 
 export default function RobotCar() {
@@ -6,12 +7,12 @@ export default function RobotCar() {
       <Header />
       <section className="section bg-dark-grey text-light-pink">
         <div className="container mx-auto py-16 px-6">
-          <h1 className="text-5xl font-bold mb-4">ROBOT CAR</h1>
+          <h1 className="text-5xl font-bold mb-4 neon">ROBOT CAR</h1>
           <div className="mb-8">
             <img
-              src="/robotcar.png"  // Assuming the image is saved as 'robotcar.jpg' in the public folder
+              src="/robotcar.png"
               alt="Robotic Car Project"
-              className="rounded-lg shadow-lg w-full h-auto"
+              className="rounded-lg shadow-lg w-3/4 h-auto mx-auto md:w-1/2" // Adjusted image size
             />
           </div>
           <p className="text-lg mb-6">
