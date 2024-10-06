02bd678086b293a53fcf923fae7be901f37458a8
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-10-06 22:00:11 +0100

    Deploy updated site

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -370,7 +370,7 @@ export default function Hero() {
           ></canvas>
 
           {/* "View My Work" button under the animation */}
-          <div className="mt-2 md:mt-4">
+          <div className="mt-1 md:mt-3">
             <Link href="/#projects" scroll={false}>
               <a
                 className="px-6 py-3 text-xl transition-transform transform hover:scale-105 view-my-work-button"
