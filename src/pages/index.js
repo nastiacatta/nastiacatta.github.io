0c8db20e0f071afe63ff970d32f47f2083571a8d
nastiacattaneo <116082737+nastiacattaneo@users.noreply.github.com>
2024-12-02 11:47:09 +0000

    Deploy updated site

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@
 import Hero from '../components/Hero';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
+import About from '../components/About'; // Importing About component
 
 export default function Home() {
   return (
